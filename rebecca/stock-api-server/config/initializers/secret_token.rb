074b237819069a4a1c7ff247f9991b932b3940ca
Rebecca <yunchu.chen@rakuten.com>
2017-02-21 10:26:31 +0800

    Init commit

---
+++ rebecca/stock-api-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'e3340adf02ab5ae97476c5f91c51575f9888d33eb14d8062a1c69b8f71de3c26dd99f2bac6122b7eaf563a3c263854d8b1a3f24bba84641f2a5303d9f140af9c'

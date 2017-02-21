b5c24f16501e794830b846567444fc0121cc1439
Rebecca <yunchu.chen@rakuten.com>
2017-02-21 17:34:03 +0800

    add search action and view

--- rebecca/stock-api-server/app/controllers/stock_controller.rb
+++ rebecca/stock-api-server/app/controllers/stock_controller.rb
@@ -3,4 +3,17 @@ class StockController < ApplicationController
     @stocks = Stock
               .where('date(created_at) = ?', Date.today)
   end
+
+  def search
+    date = Date.parse(params[:date]) unless params[:date].empty?
+    code = params[:code]
+    operator = !params[:date].empty? && !params[:code].empty? ? 'AND' : 'OR'
+
+    @stocks = Stock
+              .where("(date(created_at) = ? #{operator} stock_code = ?)", date, code)
+
+    return unless @stocks.empty?
+    flash[:alert] = "there is no data with date = #{date} and stock code = #{code}"
+    redirect_to stock_url
+  end
 end
--- rebecca/stock-api-server/config/routes.rb
+++ rebecca/stock-api-server/config/routes.rb
@@ -1,4 +1,5 @@
 App::Application.routes.draw do
   root to: 'stock#index'
   get 'stock' => 'stock#index'
+  get 'search' => 'stock#search'
 end

ccbf3ba8fb0c0213e5aac9fa4cf91692e4433f6a
PinChun-Kuo <pinery201@gmail.com>
2017-02-22 11:13:38 +0800

    fix problems based on rubocop

--- chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
+++ chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe TurnoversController, type: :controller do
       end
 
       it 'should render the index template' do
-        expect(response).to render_template("index")
+        expect(response).to render_template('index')
       end
     end
 
     context 'when query data of specified stock number and date' do
       before(:each) do
         # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
-        get :index, params: { number: 2330, date: '2017-02-17'}
+        get :index, params: { number: 2330, date: '2017-02-17' }
       end
 
       it 'should respond successfully with an HTTP 200 status code' do
@@ -29,14 +29,14 @@ RSpec.describe TurnoversController, type: :controller do
       end
 
       it 'should render the index template' do
-        expect(response).to render_template("index")
+        expect(response).to render_template('index')
       end
     end
 
     context 'when query data of specified date' do
       before(:each) do
         # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
-        get :index, params: { date: '2017-02-17'}
+        get :index, params: { date: '2017-02-17' }
       end
 
       it 'should respond successfully with an HTTP 200 status code' do
@@ -44,15 +44,14 @@ RSpec.describe TurnoversController, type: :controller do
       end
 
       it 'should render the index template' do
-        expect(response).to render_template("index")
+        expect(response).to render_template('index')
       end
-
     end
 
     context 'when query data of specified stock number over the years' do
       before(:each) do
         # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
-        get :index, params: { number: 2330}
+        get :index, params: { number: 2330 }
       end
 
       it 'should respond successfully with an HTTP 200 status code' do
@@ -60,7 +59,7 @@ RSpec.describe TurnoversController, type: :controller do
       end
 
       it 'should render the index template' do
-        expect(response).to render_template("index")
+        expect(response).to render_template('index')
       end
     end
   end
--- chloe/auto_stock_server/spec/factories/turnovers.rb
+++ chloe/auto_stock_server/spec/factories/turnovers.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
   factory :turnover, class: 'Turnover' do
     number 2330
     name '台積電'
-    hyper_link "http://stock.wearn.com/a2330.html"
+    hyper_link 'http://stock.wearn.com/a2330.html'
     opening_price 190.0
     highest_price 190.5
     lowest_price 189.0
     closing_yesterday 189.0
     closing_today 189.5
-    volumn 20373
+    volumn 20_373
     change '0.50'
     quote_change '0.26%'
     date Time.new.in_time_zone.to_date.to_s
@@ -17,13 +17,13 @@ FactoryGirl.define do
   factory :turnover_old, class: 'Turnover' do
     number 2330
     name '台積電'
-    hyper_link "http://stock.wearn.com/a2330.html"
+    hyper_link 'http://stock.wearn.com/a2330.html'
     opening_price 190.0
     highest_price 190.5
     lowest_price 189.0
     closing_yesterday 189.0
     closing_today 189.5
-    volumn 20373
+    volumn 20_373
     change '0.50'
     quote_change '0.26%'
     date '2017-02-13'

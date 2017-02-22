c3a4fe791b9b73034c70fc54f5e945e00079ab41
PinChun-Kuo <pinery201@gmail.com>
2017-02-22 09:44:36 +0800

    project init

---
+++ chloe/auto_stock_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoStockServer::Application.config.secret_token = '35c513aec681fb8a7a23fe5003248cd798137e1b1cc9b0f3c10a692cf37d372351abb00afc1a0da1e2226f42592d052dd50011eaf7936fa7cb0549114fac2f4f'

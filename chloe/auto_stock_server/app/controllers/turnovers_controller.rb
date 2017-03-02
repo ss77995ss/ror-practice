765e5fcd7a6c2a54e8442268eb480322f66900a5
PinChun-Kuo <pinery201@gmail.com>
2017-03-02 11:39:44 +0800

    modify SQL search condition and add order default

--- chloe/auto_stock_server/app/controllers/turnovers_controller.rb
+++ chloe/auto_stock_server/app/controllers/turnovers_controller.rb
@@ -2,14 +2,13 @@ class TurnoversController < ApplicationController
   def index
     @turnovers =
       if !params[:date].blank? && !params[:number].blank?
-        Turnover.where(number: params[:number], date: params[:date])
+        Turnover.where('number like ? and date = ?', "%#{params[:number]}%", params[:date]).order('volumn DESC')
       elsif !params[:date].blank?
-        Turnover.where(date: params[:date])
+        Turnover.where(date: params[:date]).order('volumn DESC')
       elsif !params[:number].blank?
-        Turnover.where(number: params[:number])
-        # Turnover.where(['number like ?', '%#{params[:number]}%'])
+        Turnover.where('number like ?', "%#{params[:number]}%").order('volumn DESC')
       else
-        Turnover.where(date: Time.new.in_time_zone.to_date.to_s)
+        Turnover.where(date: Time.new.in_time_zone.to_date.to_s).order('volumn DESC')
       end
   end
 end

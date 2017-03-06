01d7f2db8c87e34c53acc9c63ab5bd9ee6522d69
PinChun-Kuo <pinery201@gmail.com>
2017-03-06 11:09:23 +0800

    set root route

--- chloe/auto_stock_server/config/routes.rb
+++ chloe/auto_stock_server/config/routes.rb
@@ -1,5 +1,6 @@
 AutoStockServer::Application.routes.draw do
   resources :turnovers, only: [:index]
+  root to: 'turnovers#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

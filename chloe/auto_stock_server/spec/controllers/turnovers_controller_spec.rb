6063610bd81a0dabbc66c4306e14a79607a78e11
PinChun-Kuo <pinery201@gmail.com>
2017-03-07 10:28:05 +0800

    update controller test for params init method

--- chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
+++ chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe TurnoversController, type: :controller do
   describe 'GET #index' do
     context 'when query data default successfully' do
       before(:each) do
-        # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
+        allow(controller).to receive(:date_init).and_return('')
+        allow(controller).to receive(:number_init).and_return('')
         get :index
       end
 
@@ -15,7 +16,8 @@ RSpec.describe TurnoversController, type: :controller do
 
     context 'when query data of specified stock number and date' do
       before(:each) do
-        # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
+        allow(controller).to receive(:date_init).and_return('2017-02-17')
+        allow(controller).to receive(:number_init).and_return('2330')
         get :index, params: { number: 2330, date: '2017-02-17' }
       end
 
@@ -25,8 +27,9 @@ RSpec.describe TurnoversController, type: :controller do
 
     context 'when query data of specified date' do
       before(:each) do
-        # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
-        get :index, params: { date: '2017-02-17' }
+        allow(controller).to receive(:date_init).and_return('2017-02-17')
+        allow(controller).to receive(:number_init).and_return('null')
+        get :index, params: { number: 'null', date: '2017-02-17' }
       end
 
       it { expect(response).to have_http_status(200) }
@@ -35,12 +38,33 @@ RSpec.describe TurnoversController, type: :controller do
 
     context 'when query data of specified stock number over the years' do
       before(:each) do
-        # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
-        get :index, params: { number: 2330 }
+        allow(controller).to receive(:date_init).and_return('null ')
+        allow(controller).to receive(:number_init).and_return('2330')
+        get :index, params: { number: 2330, date: 'null' }
       end
 
       it { expect(response).to have_http_status(200) }
       it { expect(response).to render_template('index') }
+    end
+  end
+
+  describe '#date_init' do
+    context 'when date is null' do
+      it { expect(controller.date_init('null')).to eq(nil) }
+    end
+
+    context 'when date is not null' do
+      it { expect(controller.date_init('2017-03-03')).to eq('2017-03-03') }
+    end
+  end
+
+  describe '#number_init' do
+    context 'when date is null' do
+      it { expect(controller.number_init('null')).to eq(nil) }
+    end
+
+    context 'when date is not null' do
+      it { expect(controller.number_init('24')).to eq('24') }
     end
   end
 end

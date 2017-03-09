5b14e52d65d93ec844fe9a1bd9a21a1b64790494
PinChun-Kuo <pinery201@gmail.com>
2017-03-09 15:52:14 +0800

    add export function

--- chloe/auto_stock_server/app/assets/javascripts/application.js
+++ chloe/auto_stock_server/app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require alasql.min
+//= require xlsx.core.min
 //= require bootstrap-sprockets
 //= require angular
 //= require angular-rails-templates

3f2b800f62b3aa699e5fbd85c68466cafbebc9f1
Pinery Kuo <pinerykuo@Pineryde-MacBook-Pro.local>
2017-03-09 23:42:58 +0800

    modify promise for angular service

--- chloe/auto_stock_server/app/assets/javascripts/stocks/stock_service.js
+++ chloe/auto_stock_server/app/assets/javascripts/stocks/stock_service.js
@@ -1,9 +1,19 @@
 app.service('stockService', function() {
   this.getStocks = function($http) {
-    return $http.get('/turnovers.json');
+    return $http.get('/turnovers.json')
+      .then(function(res) {
+        return res.data;
+      }).catch( function(err) {
+        console.log(err);
+      });
   };
 
   this.searchStock = function($http, number, date) {
-    return $http.get('/turnovers.json?number=' + number + '&date=' + date);
+    return $http.get('/turnovers.json?number=' + number + '&date=' + date)
+      .then(function(res) {
+        return res.data;
+      }).catch( function(err) {
+        console.log(err);
+      });;
   };
 });

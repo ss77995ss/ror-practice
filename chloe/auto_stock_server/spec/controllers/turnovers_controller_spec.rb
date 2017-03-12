c9e8f982b1022fc6aea20362380fd3a26132d340
Pinery Kuo <pinerykuo@Pineryde-MacBook-Pro.local>
2017-03-12 17:55:58 +0800

    arrange it writing for controller test

--- chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
+++ chloe/auto_stock_server/spec/controllers/turnovers_controller_spec.rb
@@ -3,19 +3,14 @@ require_relative '../../API/crawler.rb'
 
 RSpec.describe TurnoversController, type: :controller do
   describe 'GET #index' do
-    context 'when query data default' do
+    context 'when query data default successfully' do
       before(:each) do
         # allow(Turnover).to receive(:insert_to_db).and_return(FactoryGirl.build_list(:turnover, Crawler::DATA_COUNT))
         get :index
       end
 
-      it 'should respond successfully with an HTTP 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-
-      it 'should render the index template' do
-        expect(response).to render_template('index')
-      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('index') }
     end
 
     context 'when query data of specified stock number and date' do
@@ -24,13 +19,8 @@ RSpec.describe TurnoversController, type: :controller do
         get :index, params: { number: 2330, date: '2017-02-17' }
       end
 
-      it 'should respond successfully with an HTTP 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-
-      it 'should render the index template' do
-        expect(response).to render_template('index')
-      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('index') }
     end
 
     context 'when query data of specified date' do
@@ -39,13 +29,8 @@ RSpec.describe TurnoversController, type: :controller do
         get :index, params: { date: '2017-02-17' }
       end
 
-      it 'should respond successfully with an HTTP 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-
-      it 'should render the index template' do
-        expect(response).to render_template('index')
-      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('index') }
     end
 
     context 'when query data of specified stock number over the years' do
@@ -54,13 +39,8 @@ RSpec.describe TurnoversController, type: :controller do
         get :index, params: { number: 2330 }
       end
 
-      it 'should respond successfully with an HTTP 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-
-      it 'should render the index template' do
-        expect(response).to render_template('index')
-      end
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('index') }
     end
   end
 end

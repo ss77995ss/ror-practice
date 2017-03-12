cd6feb62d9b66f970367478f33ed80800ca4980a
Pinery Kuo <pinerykuo@Pineryde-MacBook-Pro.local>
2017-03-12 17:05:45 +0800

    update controller, view, and routing test

--- chloe/auto_stock_server/spec/routing/turnovers_routing_spec.rb
+++ chloe/auto_stock_server/spec/routing/turnovers_routing_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe 'route for turnovers', type: :routing do
     expect(get: '/turnovers')
       .to route_to('turnovers#index')
   end
+
+  it 'route /turnovers/search to the turnovers controller' do
+    expect(get: '/turnovers/search')
+      .to route_to('turnovers#search')
+  end
 end
---
+++ chloe/auto_stock_server/spec/view/turnovers/index.html.slim_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require_relative '../../../API/crawler.rb'
+
+RSpec.describe 'turnovers/index.html.slim', type: :view do
+  describe '' do
+    before(:each) do
+      @turnovers = FactoryGirl.build_list(:turnover, 2)
+      assign(:turnovers, @turnovers)
+      render
+    end
+
+    it 'should have have these attributes' do
+      expect(response).to have_css("div[ng-app='turnoversApp'][ng-controller='TurnoverCtrl']", class: 'turnovers')
+    end
+  end
+
+  describe 'search directive' do
+    before(:each) do
+      render
+    end
+
+    it 'should exist' do
+      expect(response).to have_css('search')
+    end
+
+    it 'should have have these attributes' do
+      expect(response).to have_css("search[stock-number='number'][search-date='date'][search-function='search()'][export-function='export()']")
+    end
+  end
+
+  describe 'stock-table directive' do
+    before(:each) do
+      render
+    end
+
+    it 'should exist' do
+      expect(response).to have_css('stock-table')
+    end
+
+    it 'should have have these attributes' do
+      expect(response).to have_css("stock-table[desc='desc'][asc='asc'][turnovers='turnovers'][sortdesc='sortDesc()'][sortasc='sortAsc()']")
+    end
+  end
+end

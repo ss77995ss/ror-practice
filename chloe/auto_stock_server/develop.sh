a9a8a1267c48c1e8b862485170dec767c8d19139
PinChun-Kuo <pinery201@gmail.com>
2017-03-14 15:13:33 +0800

    migrate database before stsrt up server

--- chloe/auto_stock_server/develop.sh
+++ chloe/auto_stock_server/develop.sh
@@ -2,4 +2,4 @@
 docker run -it -P -v /Users/pinchun.kuo/Desktop/ror-practice/chloe/auto_stock_server:/auto_stock_server --name ror-practice-dev -p 3000:3000 -p 9876:9876 -d chloe/ror-practice-dev
 
 # start the server
-docker exec -it ror-practice-dev /bin/bash -c "./whenever.sh; rails s"
+docker exec -it ror-practice-dev /bin/bash -c "./whenever.sh; rake db:migrate; rails s"

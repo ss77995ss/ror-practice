0ec8a23bcb23ebadca63ca551580db9bf66e81b9
PinChun-Kuo <pinery201@gmail.com>
2017-03-14 15:11:15 +0800

    test environment build

--- chloe/auto_stock_server/develop.sh
+++ chloe/auto_stock_server/develop.sh
@@ -1,5 +1,5 @@
 # link both of them and create database by postgres role first
-docker run -it -P -v /Users/pinchun.kuo/Desktop/ror-practice/chloe/auto_stock_server:/auto_stock_server --name ror-practice-dev -p 3000:3000 -d chloe/ror-practice-dev
+docker run -it -P -v /Users/pinchun.kuo/Desktop/ror-practice/chloe/auto_stock_server:/auto_stock_server --name ror-practice-dev -p 3000:3000 -p 9876:9876 -d chloe/ror-practice-dev
 
 # start the server
 docker exec -it ror-practice-dev /bin/bash -c "./whenever.sh; rails s"

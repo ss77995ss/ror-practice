5bd0b12f1e3cd2e0c4d521e576e02aec87f228c7
Hsieh, LiHsuan | Sean | TWR <lihsuan.hsieh@rakuten.com>
2018-12-24 16:08:05 +0800

    Init setting

---
+++ sean/stock_api_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApiServer::Application.config.secret_token = 'b6e925654818d34dbfde0e2ca4c815c955e3fd1879cf02435c31cafd9fb205d1cf2aa31e758f6cea4a96d902f1ced664ca6ccafcf8bc343d400ba68885e81384'
